459420057d9f6bc5260589e7fd32f9fe7213a6f3
Adrian-Stefan Mares <adrianm97@gmail.com>
2023-08-22 20:31:13 +0200

    Add proxy authorization

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/adriansmares/connect
 
 go 1.20
 
-require golang.org/x/sync v0.3.0
+require (
+	golang.org/x/crypto v0.12.0
+	golang.org/x/sync v0.3.0
+)
+
+require golang.org/x/sys v0.11.0 // indirect
--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"crypto/subtle"
+	"encoding/base64"
 	"io"
 	"log"
 	"net"
 	"net/http"
+	"os"
+	"strings"
 	"sync/atomic"
+	"time"
 
+	"golang.org/x/crypto/sha3"
 	"golang.org/x/sync/errgroup"
 )
 
@@ -19,7 +25,51 @@ func pipe(dst io.WriteCloser, src io.Reader) func() error {
 	}
 }
 
+func parseBasicAuth(auth string) (username, password string, ok bool) {
+	const prefix = "Basic "
+	if len(auth) < len(prefix) || !strings.EqualFold(auth[:len(prefix)], prefix) {
+		return "", "", false
+	}
+	c, err := base64.StdEncoding.DecodeString(auth[len(prefix):])
+	if err != nil {
+		return "", "", false
+	}
+	cs := string(c)
+	username, password, ok = strings.Cut(cs, ":")
+	if !ok {
+		return "", "", false
+	}
+	return username, password, true
+}
+
+const (
+	proxyAuthenticate  = "Proxy-Authenticate"
+	proxyAuthorization = "Proxy-Authorization"
+)
+
+func handleAuthentication(w http.ResponseWriter, r *http.Request, expectedUsernameHash, expectedPasswordHash [64]byte, requireAuth bool) bool {
+	if !requireAuth {
+		return true
+	}
+	username, password, ok := parseBasicAuth(r.Header.Get(proxyAuthorization))
+	if !ok {
+		return false
+	}
+	usernameHash, passwordHash := sha3.Sum512([]byte(username)), sha3.Sum512([]byte(password))
+	equalUsername := subtle.ConstantTimeCompare(usernameHash[:], expectedUsernameHash[:])
+	equalPassword := subtle.ConstantTimeCompare(passwordHash[:], expectedPasswordHash[:])
+	return (equalUsername & equalPassword) == 1
+}
+
 func main() {
+	usernameEnv, hasUsername := os.LookupEnv("PROXY_USERNAME")
+	passwordEnv, hasPassword := os.LookupEnv("PROXY_PASSWORD")
+	usernameHash, passwordHash := sha3.Sum512([]byte(usernameEnv)), sha3.Sum512([]byte(passwordEnv))
+	requireAuth := hasUsername && hasPassword
+	if requireAuth {
+		log.Println("authentication required")
+	}
+
 	id := uint64(0)
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		id := atomic.AddUint64(&id, 1)
@@ -31,7 +81,21 @@ func main() {
 			return
 		}
 
-		serverConn, err := (&net.Dialer{}).DialContext(r.Context(), "tcp", r.Host)
+		if !handleAuthentication(w, r, usernameHash, passwordHash, requireAuth) {
+			log.Println(id, "authentication failed")
+			w.Header().Add(proxyAuthenticate, "Basic")
+			http.Error(w, "authentication required", http.StatusProxyAuthRequired)
+			return
+		}
+
+		host, port, err := net.SplitHostPort(r.Host)
+		if err != nil {
+			log.Println(id, "split host port failed", err)
+			http.Error(w, "invalid host", http.StatusBadRequest)
+			return
+		}
+
+		serverConn, err := (&net.Dialer{}).DialContext(r.Context(), "tcp", net.JoinHostPort(host, port))
 		if err != nil {
 			log.Println(id, "dial failed", err)
 			http.Error(w, "dial failed", http.StatusServiceUnavailable)
@@ -39,7 +103,6 @@ func main() {
 		}
 		defer serverConn.Close()
 		defer io.Copy(io.Discard, serverConn) // nolint:errcheck
-		log.Println(id, "dial done")
 
 		hijacker, ok := w.(http.Hijacker)
 		if !ok {
@@ -57,7 +120,11 @@ func main() {
 		}
 		defer clientConn.Close()
 		defer io.Copy(io.Discard, clientConn) // nolint:errcheck
-		log.Println(id, "hijack done")
+
+		if err := clientConn.SetDeadline(time.Time{}); err != nil {
+			log.Println(id, "set deadline failed", err)
+			return
+		}
 
 		group := errgroup.Group{}
 		group.Go(pipe(serverConn, clientConn))
